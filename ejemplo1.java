da6432c019a05476edd9c6de81fa1fe9361b42bd
smrv21-10 <smrv21-10@c16x14.iescierva.net>
2024-01-31 17:59:11 +0100

    EJEM1 Modificar mensaje

--- ejemplo1.java
+++ ejemplo1.java
@@ -2,6 +2,6 @@ public class ejemplo1 {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         String nombre = "David";
-        System.out.println("Mi nombre es " + nombre);
+        System.out.println("Mi nombre es  ------ > " + nombre );
     }
 }
