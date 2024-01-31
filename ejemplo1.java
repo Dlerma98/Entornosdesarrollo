8582fddc9e0ab2ce4f9a4dda72306f06a9e38f6c
David Garcia Lerma <158084769+Dlerma98@users.noreply.github.com>
2024-01-31 19:10:24 +0100

    Update ejemplo1.java

--- ejemplo1.java
+++ ejemplo1.java
@@ -3,5 +3,6 @@ public class ejemplo1 {
         System.out.println("Hola Mundo");
         String nombre = "David";
         System.out.println("Mi nombre es  ------ > " + nombre );
+        System.out.println("La suma de 2 y 3 es " + (2+3));
     }
 }
